6384c87d4383be96775be0f27a3a6e1b77a2a23d
carmmcl <carolina.garcia@mail.com>
2022-08-23 21:52:08 -0500

    correccones

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './Navbar.css';
+import './NavBar.css';
 import {CartWidget} from './CartWidget';
 
 function NavBar() {
   return (
     <>
       <Navbar bg="pink" variant="dark">
-        <Container className='container-navBar'>
+        <Container className='container-NavBar'>
           <Navbar.Brand href="#home">
             <img src='./assets/logo2.png'></img>
           </Navbar.Brand>
