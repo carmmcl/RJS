32c0bb93122fdc909d7372c7c4eebb2648167b0b
carmmcl <carolina.garcia@mail.com>
2022-08-24 11:03:25 -0500

    correcciones

--- src/components/ItemlistContainer/ItemListContainer.js
+++ src/components/ItemlistContainer/ItemListContainer.js
@@ -3,7 +3,7 @@ import './ItemListContainer.css'
 export const ItemListContainer = (props)=> {
     
     return (
-        <h5 className="titulo">Best Cupcakes-{props.greeting}</h5>
+        <h5 className="titulo">Best Cupcakes - {props.greeting}</h5>
     )
 
 }
