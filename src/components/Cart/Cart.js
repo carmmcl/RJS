a034a77762cc163f2737968f3e935f2ca5316160
carmmcl <carolina.garcia@mail.com>
2022-09-19 08:47:53 -0500

    inCarrito vacío

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,23 @@ import { useContext } from "react"
 import { CartContext } from "../Context/CartContext"
 import { Button } from "reactstrap"
 import {BsFillTrashFill} from "react-icons/bs";
+import {Link} from 'react-router-dom'
 
 
 export const Cart =()=>{
 
     const { cart, cartTotal, emptyCart, removeItem} = useContext (CartContext)
+
+    if (cart.length === 0){
+
+        return (
+            <div className="container-my 5">
+                <h2>No hay productos en tu carrito</h2>
+                <hr/>
+                <Link to="/" className="btn btn-primary">Ir a comprar</Link>
+            </div>
+        )
+    }
 
     return(
 
